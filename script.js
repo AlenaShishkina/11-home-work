3b8f8637f2b089794443e3d12fc075cb9e9487ab
AlenaShishkina <79803218+AlenaShishkina@users.noreply.github.com>
2021-03-28 20:00:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var classArray = ["firstClass", "SecondClass", "ThirdClass", "firstClass", "SecondClass", "ThirdClass"];
+var xx = document.getElementById("xxx");
+
+function setColorClass(id) {
+  let elem = document.getElementById(id);
+  if (!elem.hasAttribute("class")) {
+    elem.setAttribute("class", classArray[0]);
+    classArray.shift();
+    let num = 6 - classArray.length;
+    elem.setAttribute("number", num);
+    elem.innerHTML = num;
+  }
+}
+for (let i = 0; i < 6; i++) {
+  let btn = document.createElement("BUTTON");
+  btn.setAttribute("id", i);
+  btn.setAttribute("onclick", "setColorClass('" + i + "')");
+  xx.appendChild(btn);
+
+}
